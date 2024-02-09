4a71df736372f6c041b9a2bc6a870c4e6e846b3c
Willow-Zero <zmckinnis4@gmail.com>
2024-02-09 11:38:41 -0500

    adding config file.

---
+++ config.py
@@ -0,0 +1,75 @@
+#This is the config file for TimeTableWidget. The format is as follows
+#CITYlinenameStops=["examplestop"]
+# note that these need to be the exact stop names in the apis, unless ive futzed with the way the program interprets things
+# ill try to provide a list of the valid stop names for each service i implement
+# if you give no stops (the dict is empty) nothing will be done. 
+# also at the end theres the scroll mode config stuff, ill comment on that when i get to it
+# also there are of course some other config options, ill comment on those when they show up
+# please, enjoy my crappy widget
+
+#NYC rail services
+NYCAStops=[]
+NYCBStops=[]
+NYCCStops=[]
+NYCDStops=[]
+NYCEStops=[]
+NYCFStops=[]
+#note:X indicates express service
+NYCFXStops=[]
+NYCGStops=[]
+NYCJStops=[]
+NYCLStops=[]
+NYCMStops=[]
+NYCNStops=[]
+NYCQStops=[]
+NYCRStops=[]
+NYCWStops=[]
+NYCSStops=[]
+NYCTStops=[]
+NYCWStops=[]
+NYCZStops=[]
+NYC1Stops=[]
+NYC2Stops=[]
+NYC3Stops=[]
+NYC4Stops=[]
+NYC5Stops=[]
+NYC6Stops=[]
+NYC7Stops=[]
+NYC6Stops=[]
+NYC7Stops=[]
+#Staten Island Railroad
+NYCSIRStops=[]
+#PATH lines (order is:Newark-World Trade, Hoboken-World Trade,Journal Square-33rd, Hoboken-33rd)
+PATHNWKWTCStops=[]
+PATHHOBWTCStops=[]
+PATHJSQ33Stops=[]
+PATHHOB33Stops=[]=True
+#Metro-North(MN) Lines
+MNPVLStops=[] #Pascack Valley Line      
+MNPJLStops=[] #Port Jervis Line
+MNHaLStops=[] #Harlem Line      
+MNHuLStops=[] #Hudson Line
+MNNHLStops=[] #New Haven Line 
+#LIRR
+LIRRMLStops=[] #Mail Line
+LIRRABStops=[] #Atlantic Branch   
+LIRRMBStops=[] #Montauk Branch
+LIRRBBStops=[] #Babylon Branch   
+LIRRBPBStops=[] #Belmont Park Branch 
+LIRRCBStops=[] #Central Branch   
+LIRRCTZStops=[] #City Terminal Zone   
+LIRRFRBStops=[] #Far Rockaway Branch   
+LIRRHBStops=[] #Hempstead Branch   
+LIRRLBBStops=[] #Long Beach Branch   
+LIRROBBStops=[] #Oyster Bay Branch   
+LIRRPJBStops=[] #Port Jefferson Branch   
+LIRRPWBStops=[] #Port Washington Branch   
+LIRRRkBStops=[] #Ronkonkoma Branch   
+LIRRWHBStops=[] #West Hempstead Branch
+ 
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 #TimeTableWidget
+
 The idea of this project is to put together a way to display the timetables for transit services in an aesthetically pleasing manner, while also providing useful information. form factor inspired by a device a friend of mine has that i cannot for the life of me track down, but which does essentially this, as well as some other stuff that i wouldnt necessarily find as useful - though i may impliment some of these later, and turn this into a more general info widget. 
