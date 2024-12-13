28e779763ecaf0f847f8648ca2d33d9a5f3444a7
Willow-Zero <zmckinnis4@gmail.com>
2024-12-13 01:14:17 -0500

    pushing some changes

--- universalUse.py
+++ universalUse.py
@@ -1,16 +1,18 @@
 import time
 
 def printIcon(icon,stationName,date,arrivalTime,departureTime,destination,heading,colorcode):
-        g = max(len(stationName),len(date),len(arrivalTime),len(destination),16,len(heading))
+        g = 31 #89x24 assumed size, TODO: improve sizing 
+        if len(stationName) > g:
+                stationName = stationName[0:31]
         bar = "═"*(g+1)
-        print("\033[2╔═══════════════════════════════════════════════════╦═" + bar + "╗")
+        print("\033[2J╔═════════════════════════════════════════════════╦" + bar + "╗")
         dataArray = ["Station Name:".ljust(g),stationName.ljust(g),bar,"Date:".ljust(g),date.ljust(g),bar,"Arrival Time:".ljust(g),arrivalTime.ljust(g),bar,"Departure Time:".ljust(g),departureTime.ljust(g).ljust(g),bar,"Destination:".ljust(g),destination.ljust(g),bar,"Heading:".ljust(g),heading.ljust(g),bar,"".ljust(g),"".ljust(g),"".ljust(g),"","","","","","","","","","","","","","","",""]
         i=0
-        y = [" ║ "," ║ "," ╠═"," ║ "," ║ "," ╠═"," ║ "," ║ "," ╠═"," ║ "," ║ "," ╠═"," ║ "," ║ "," ╠═"," ║ "," ║ "," ╠═"," ║ "," ║ "," ║ "]
+        y = [" ║"," ║"," ╠"," ║"," ║"," ╠"," ║"," ║"," ╠"," ║"," ║"," ╠"," ║"," ║"," ╠"," ║"," ║"," ╠"," ║"," ║"," ║"]
         u = [" ║ "," ║ ","╣"," ║ "," ║ ","╣"," ║ "," ║ ","╣"," ║ "," ║ ","╣"," ║ "," ║ ","╣"," ║ "," ║ ","╣"," ║ "," ║ "," ║"]
         for x in icon:
                 print("║" + colorcode + x + "\x1b[0m" + y[i] + dataArray[i]+ u[i])
                 i = i+1
-        print("╚═══════════════════════════════════════════════════╩═" + bar + "╝")
-        time.sleep(5)
+        print("╚═════════════════════════════════════════════════╩" + bar + "╝")
+        time.sleep(2.5)
 
